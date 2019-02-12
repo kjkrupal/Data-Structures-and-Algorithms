ed8b936f3b034449352d95a013c82598bcb8d4d1
kjkrupal <kjkrupal@gmail.com>
2019-02-11 23:15:49 -0800

    Heap sort

--- Heap/src/Implementation.java
+++ Heap/src/Implementation.java
@@ -1,30 +1,39 @@
+import java.util.Arrays;
+
 public class Implementation {
     public static void main(String[] args) throws Exception{
 
         int[] arr = {27, 2, 48, 19, 13, 45, 61, 30, 76, 4};
+
+        //Build heap by manually inserting element
         MaxHeap heap = new MaxHeap();
 
         for(int i = 0; i < arr.length; i++){
             heap.add(arr[i]);
         }
 
-        System.out.println(heap.getHeap() + "\n");
+        System.out.println(heap.toString() + "\n");
         System.out.println(heap.extractMax());
-        System.out.println(heap.getHeap() + "\n");
+        System.out.println(heap.toString() + "\n");
         System.out.println(heap.extractMax());
-        System.out.println(heap.getHeap() + "\n");
+        System.out.println(heap.toString() + "\n");
         System.out.println(heap.extractMax());
-        System.out.println(heap.getHeap() + "\n");
+        System.out.println(heap.toString() + "\n");
         System.out.println(heap.extractMax());
-        System.out.println(heap.getHeap() + "\n");
+        System.out.println(heap.toString() + "\n");
         System.out.println(heap.extractMax());
-        System.out.println(heap.getHeap() + "\n");
+        System.out.println(heap.toString() + "\n");
         System.out.println(heap.extractMax());
-        System.out.println(heap.getHeap() + "\n");
+        System.out.println(heap.toString() + "\n");
         System.out.println(heap.extractMax());
-        System.out.println(heap.getHeap() + "\n");
+        System.out.println(heap.toString() + "\n");
         System.out.println(heap.extractMax());
-        System.out.println(heap.getHeap() + "\n");
+        System.out.println(heap.toString() + "\n");
+
+        //Build heap from an array
+        MaxHeap heap_arr = new MaxHeap(arr);
+
+        System.out.println(Arrays.toString(heap_arr.sort()));
 
     }
 }
--- Heap/src/MaxHeap.java
+++ Heap/src/MaxHeap.java
@@ -6,6 +6,8 @@ public class MaxHeap {
     private int size;
     private int maxSize;
 
+
+
     public MaxHeap(){
         heap = new int[10];
         this.maxSize = 10;
@@ -16,6 +18,13 @@ public class MaxHeap {
         this.maxSize = initialCapacity;
     }
 
+    public MaxHeap(int[] arr){
+        heap = arr;
+        this.size = heap.length;
+        this.maxSize = heap.length;
+        buildHeap();
+    }
+
     private void shiftUp(int i){
         while(heap[parent(i)] < heap[i] && i > 0){
             swap(parent(i), i);
@@ -23,14 +32,20 @@ public class MaxHeap {
         }
     }
 
+    private void buildHeap(){
+        for(int i = (size/2) - 1; i >= 0; i --){
+            shiftDown(i);
+        }
+    }
+
     private void shiftDown(int i){
         int maxIndex = i;
         int l = leftChild(i);
-        if(l <= size && heap[l] > heap[maxIndex]){
+        if(l < size && heap[l] > heap[maxIndex]){
             maxIndex = l;
         }
         int r = rightChild(i);
-        if(r <= size && heap[r] > heap[maxIndex]){
+        if(r < size && heap[r] > heap[maxIndex]){
             maxIndex = r;
         }
         if(i != maxIndex){
@@ -57,7 +72,7 @@ public class MaxHeap {
         heap[j] = temp;
     }
 
-    public void add(int element) {
+    public void add(int element){
         if(size == maxSize){
             heap = Arrays.copyOf(heap, heap.length * 2);
             this.maxSize *= 2;
@@ -68,8 +83,8 @@ public class MaxHeap {
     }
 
 
-    public String getHeap(){
-        return Arrays.toString(Arrays.copyOfRange(heap, 0, size));
+    public int[] getHeap(){
+        return this.heap;
     }
 
     public int extractMax() throws EmptyHeapException{
@@ -83,4 +98,18 @@ public class MaxHeap {
         return max;
     }
 
+    public int[] sort(){
+        size--;
+        for (int i = maxSize - 1; i >= 0; i--){
+            swap(0, size);
+            size = size - 1;
+            shiftDown(0);
+        }
+        return heap;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(Arrays.copyOfRange(heap, 0, size));
+    }
 }
