bd5cca659663cb3e99a0b89e32bc346720a83b91
kjkrupal <kjkrupal@gmail.com>
2019-03-05 16:14:35 -0800

    Predicate 3

--- JAVA 8/src/Predicate/Main.java
+++ JAVA 8/src/Predicate/Main.java
@@ -1,8 +1,14 @@
 package Predicate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 import java.util.function.Predicate;
 
 public class Main {
+
+    static List list = Arrays.asList(new int[]{1,2,3});
 
     public static void main(String[] args) {
 
@@ -15,6 +21,10 @@ public class Main {
         Predicate <String> strGreatFive = s -> s.length() > 5;
         System.out.println(strGreatFive.test("krupal"));
         System.out.println(strGreatFive.test("nice"));
+
+        // Predicate to check collection is empty or not
+        Predicate <Collection> collectionIsEmpty = c -> c.isEmpty();
+        System.out.println(collectionIsEmpty.test(list));
     }
 
 }
