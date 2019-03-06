ab97f17615af1b8f0ea2f1b1556aa5fad24da79f
kjkrupal <kjkrupal@gmail.com>
2019-03-05 16:07:27 -0800

    Predicate 2

--- JAVA 8/src/Predicate/Main.java
+++ JAVA 8/src/Predicate/Main.java
@@ -11,6 +11,10 @@ public class Main {
         System.out.println(greatTen.test(100));
         System.out.println(greatTen.test(5));
 
+        // Predicate to check string length greater than 5 or not
+        Predicate <String> strGreatFive = s -> s.length() > 5;
+        System.out.println(strGreatFive.test("krupal"));
+        System.out.println(strGreatFive.test("nice"));
     }
 
 }
