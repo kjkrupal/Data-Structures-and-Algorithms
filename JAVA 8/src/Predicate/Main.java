9cb29d131ed2cba601831d192edbdb8a0080923e
kjkrupal <kjkrupal@gmail.com>
2019-03-06 15:53:12 -0800

    Predicate 4

--- JAVA 8/src/Predicate/Main.java
+++ JAVA 8/src/Predicate/Main.java
@@ -12,6 +12,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String[] names = {"krupal", "tanvi", "", "sheryl", null, "bhavesh", null, "", "hitesh"};
+
         // Predicate to check number greter than 10 or not
         Predicate <Integer> greatTen = i -> i > 10;
         System.out.println(greatTen.test(100));
@@ -25,6 +27,17 @@ public class Main {
         // Predicate to check collection is empty or not
         Predicate <Collection> collectionIsEmpty = c -> c.isEmpty();
         System.out.println(collectionIsEmpty.test(list));
+
+        // Predicate to remove null and empty string from string array
+        Predicate <String> emptyStringPredicate = s -> s != null && s.length() != 0;
+        ArrayList <String> nameList = new ArrayList<>();
+        for(String s: names){
+            if(emptyStringPredicate.test(s))
+                nameList.add(s);
+        }
+        System.out.println("Proper strings are: " + nameList);
+
+        // Predicate for user authentication
     }
 
 }
