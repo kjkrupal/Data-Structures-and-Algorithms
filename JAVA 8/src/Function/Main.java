d51e3f94912ed6fc565a1862c0727bdbdad7c440
kjkrupal <kjkrupal@gmail.com>
2019-03-06 18:41:38 -0800

    Function 2

--- JAVA 8/src/Function/Main.java
+++ JAVA 8/src/Function/Main.java
@@ -5,10 +5,14 @@ import java.util.function.Function;
 public class Main {
     public static void main(String[] args) {
 
-        // Fuction to get length of string
+        // Function to get length of string
         Function<String, Integer> stringLengthFunction = s -> s.length();
         System.out.println(stringLengthFunction.apply("krupal"));
         System.out.println(stringLengthFunction.apply("I love JAVA"));
 
+        // Function to get square of number
+        Function <Integer, Integer> intSquareFunction = i -> i * i;
+        System.out.println(intSquareFunction.apply(10));
+        System.out.println(intSquareFunction.apply(27));
     }
 }
