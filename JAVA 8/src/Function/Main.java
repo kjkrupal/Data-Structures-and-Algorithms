97f8706864c87cb7e26f43e6894ee2350346cdc6
kjkrupal <kjkrupal@gmail.com>
2019-03-06 18:54:30 -0800

    Function 5

--- JAVA 8/src/Function/Main.java
+++ JAVA 8/src/Function/Main.java
@@ -24,5 +24,11 @@ public class Main {
         // Function to get number of spaces in string
         Function <String, Integer> getSpaceCountFunction = s -> s.length() - s.replaceAll(" ", "").length();
         System.out.println(getSpaceCountFunction.apply(name));
+
+        // Function to get grade of student based on marks
+        Function <Integer, Character> getGradeFunction = m -> m >= 80 ? 'A' : m >= 60 ? 'B' : m >= 40 ? 'C': 'F';
+        System.out.println(getGradeFunction.apply(91));
+        System.out.println(getGradeFunction.apply((26)));
+        System.out.println(getGradeFunction.apply(69));
     }
 }
