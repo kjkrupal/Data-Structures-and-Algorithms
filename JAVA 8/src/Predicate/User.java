986fb6e4c7db35bba0f760220c0d37481d8d3fbe
kjkrupal <kjkrupal@gmail.com>
2019-03-06 18:33:01 -0800

    Predicate 6

---
+++ JAVA 8/src/Predicate/User.java
@@ -0,0 +1,11 @@
+package Predicate;
+
+public class User {
+    String username;
+    String password;
+
+    User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
