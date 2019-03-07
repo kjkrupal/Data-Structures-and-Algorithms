2c15b6341804fd07cfeaa9ac254a9a8ec1a83ed4
kjkrupal <kjkrupal@gmail.com>
2019-03-06 18:48:00 -0800

    Function 3 & 4

--- JAVA 8/src/Function/Main.java
+++ JAVA 8/src/Function/Main.java
@@ -5,6 +5,8 @@ import java.util.function.Function;
 public class Main {
     public static void main(String[] args) {
 
+        String name = "Krupal Jadhav       Mumbai";
+
         // Function to get length of string
         Function<String, Integer> stringLengthFunction = s -> s.length();
         System.out.println(stringLengthFunction.apply("krupal"));
@@ -14,5 +16,13 @@ public class Main {
         Function <Integer, Integer> intSquareFunction = i -> i * i;
         System.out.println(intSquareFunction.apply(10));
         System.out.println(intSquareFunction.apply(27));
+
+        // Function to remove string spaces
+        Function <String, String> removeSpaceFunction = s -> s.replaceAll(" ", "");
+        System.out.println(removeSpaceFunction.apply(name));
+
+        // Function to get number of spaces in string
+        Function <String, Integer> getSpaceCountFunction = s -> s.length() - s.replaceAll(" ", "").length();
+        System.out.println(getSpaceCountFunction.apply(name));
     }
 }
