fa3be43cf63a77fc72c8b6e5303fa6bd1b3aedec
kjkrupal <kjkrupal@gmail.com>
2019-03-09 13:41:08 -0800

    Consumer 2

--- JAVA 8/src/Consumer/Main.java
+++ JAVA 8/src/Consumer/Main.java
@@ -6,8 +6,16 @@ public class Main {
     public static void main(String[] args) {
 
         // Print a string using consumer
-        Consumer<String> printString = s -> System.out.println(s);
+        Consumer <String> printString = s -> System.out.println(s);
         printString.accept("Hello");
+
+        // Consumer chaining demo
+        Consumer <String> c1 = s -> System.out.println("Hey its starting");
+        Consumer <String> c2 = s -> System.out.println(s);
+        Consumer <String> c3 = s -> System.out.println("It ended");
+
+        c1.andThen(c2).andThen(c3).accept("Consumer chaining");
+
 
     }
 }
