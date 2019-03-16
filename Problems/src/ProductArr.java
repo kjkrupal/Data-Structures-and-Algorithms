11943f7c0eaa3edcb746a8b5ad1941430792e38a
kjkrupal <kjkrupal@gmail.com>
2019-03-16 14:01:16 -0700

    Division

--- Problems/src/ProductArr.java
+++ Problems/src/ProductArr.java
@@ -13,44 +13,33 @@ public class ProductArr {
     }
 
     public static int[] getDivision(int[] arr) {
-
         int[] prod = new int[arr.length];
-
         int product = 1;
-
         for(int i = 0; i < arr.length; i++) {
             product = product * arr[i];
         }
-
         for(int i = 0; i < arr.length; i++) {
             prod[i] = product / arr[i];
         }
-
         return prod;
     }
 
     public static int[] getWithoutDivision(int[] arr){
-
         int n = arr.length;
         int[] left = new int[arr.length];
         int[] right = new int[arr.length];
         int[] prod = new int[arr.length];
         left[0] = 1;
         right[n - 1] = 1;
-
         for (int i = 1; i < n; i++) {
             left[i] = arr[i - 1] * left[i - 1];
         }
-
         for (int i = n - 2; i >= 0; i--) {
             right[i] = arr[i + 1] * right[i + 1];
         }
-
         for (int i = 0; i < n; i++) {
             prod[i] = left[i] * right[i];
         }
-
         return prod;
-
     }
 }
